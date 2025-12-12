db7f80df502f64442574cb04f46138f787bc4424
Vishal <korada.vishal.phe22@itbhu.ac.in>
2025-12-12 17:11:04 +0530

    implementing echo

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,18 @@ int main() {
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-
+  vector<string> shell_commands;
+  shell_commands.emplace_back("exit");
+  shell_commands.emplace_back("echo");
   while(true) {
     std::cout << "$ ";
     string command;
     getline(cin, command);
-    if (command == "exit") {
+    if (command.substr(0, 4) == shell_commands[1]) {
+      cout << command.substr(5) << "\n";
+      continue;
+    }
+    if (command == shell_commands[0]) {
       break;
     }
     cout << command << ": command not found\n";
