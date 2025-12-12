b32b3256b0042677814cc6ba197db0c6b0e7205f
Vishal <korada.vishal.phe22@itbhu.ac.in>
2025-12-12 17:05:08 +0530

    building repl

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,11 @@ int main() {
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
-  string command;
-  getline(cin, command);
-  cout << command<< ": command not found\n";
+
+  while(true) {
+    std::cout << "$ ";
+    string command;
+    getline(cin, command);
+    cout << command << ": command not found\n";
+  }
 }
